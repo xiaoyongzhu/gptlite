367569786e9fb7a17fc9ae7697f516e4724bd3ad
Xiaoyong Zhu <xiaoyongzhu@users.noreply.github.com>
2023-05-23 22:30:55 +0000

    add warning

--- src/components/ChatGPT/index.tsx
+++ src/components/ChatGPT/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { ChatGPTProps, ChatRole } from './interface'
 import MessageItem from './MessageItem'
@@ -10,6 +10,19 @@ import 'highlight.js/styles/atom-one-dark.css'
 
 const ChatGPT = (props: ChatGPTProps) => {
   const { loading, disabled, messages, currentMessage, onSend, onClear, onStop } = useChatGPT(props)
+
+  const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+    event.preventDefault()
+    event.returnValue = 'Are you sure you want to leave?'
+  }
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleBeforeUnload)
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload)
+    }
+  }, [])
 
   return (
     <div className="chat-wrapper">
